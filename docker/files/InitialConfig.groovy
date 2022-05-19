cb56b82b3096c35e97288355c9a313c13b912e55
LocTran <tranvanloc412@gmail.com>
2022-05-19 16:56:19 +0700

    first commit

---
+++ docker/files/InitialConfig.groovy
@@ -0,0 +1,11 @@
+import jenkins.model.*
+import jenkins.install.InstallState
+
+Jenkins.instance.setNumExecutors(0)
+Jenkins.instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
+
+// Setting the Jenkins URL
+url = System.env.JENKINS_URL
+urlConfig = JenkinsLocationConfiguration.get()
+urlConfig.setUrl(url)
+urlConfig.save()
